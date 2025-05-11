6e492ba0d59ee6839ab48930fa2a019f3b341bf5
Akabarali <dj.akbarali2002@gmail.com>
2025-05-11 17:47:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let body = document.querySelector("body");
+let h1 = document.querySelector("#exp")
+let btn = document.querySelector("#get")
+let nav = document.querySelector(".nav");
+let btn2 = document.querySelector(".btn_login");
+h1.textContent = "Explore Features";
+btn.textContent = "Get Started";
+body.appendChild(h1);
+btn.style.backgroundColor = "greey";
+btn.style.color = "rgb(98, 98, 98)";
+btn.style.border = "none";
+h1.style.border = "none";
+h1.style.color = "white";
+h1.style.backgroundColor = "black";
+h1.style.borderRadius = "5px";
+btn.style.borderRadius = "5px";
+nav.style.display = "flex";
+nav.style.justifyContent = "space-between";
+nav.style.alignItems = "center";
+btn2.style.display = "flex";
+btn2.style.alignItems = "center";
+btn2.style.width = "200px";
+
